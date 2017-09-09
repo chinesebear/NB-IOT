57ec15f643fd1dabeaae6dd0703a6405d2dff550
chinesebear <swubear@163.com>
2017-09-09 11:24:38 +0000

    add ncofig nsocr nsost nsorf

--- rtt2.1/bsp/smartloong/drivers/nb-iot.c
+++ rtt2.1/bsp/smartloong/drivers/nb-iot.c
@@ -13,8 +13,8 @@
 #include "ls1c.h"
 #include "nb-iot.h"
 static rt_device_t NB_IOT_UART = NULL;
-static rt_err_t found_uart3(rt_device_t* NBdev)
-{
+static int socketId;
+static rt_err_t found_uart3(rt_device_t* NBdev){
 	rt_device_t newdev;
 	rt_err_t iRet = RT_EOK;
 	newdev = rt_device_find("uart3");
@@ -43,7 +43,19 @@ static rt_err_t found_str(char* str,const char* substr,int*subendpos)
 	}
 	return ret;
 }
-
+static int str2int(const char* str)
+{
+	int len = strlen(str);
+	int value;
+	int i;
+	for(i=0;i<len;i++)
+	{
+		if(*(str+i) <'0' || *(str+i) > '9')return 0;
+		value += (*(str+i) - '0')*(10^(len-1));
+	}
+	if(value >512) return 0;// NB-IOT message max len=512
+	return value;
+}
 static rt_err_t SendData(rt_uint8_t* data,int len)
 {
 	rt_err_t ret = rt_device_write(NB_IOT_UART,0,data,len);
@@ -139,6 +151,17 @@ static rt_err_t AT_CSCON(void)
 {
 
 }
+static rt_err_t AT_NCONFIG(void)
+{
+	rt_err_t ret;
+	char* cmd = "AT+NCONFIG=AUTOCONNECT,TRUE\r\n";
+	char resp[256],rlen=256,int pos;
+
+	SendData(cmd,strlen(cmd));
+	ret = RecvData(resp,&rlen,2000);
+	if(ret)return ret;
+	return found_str(resp,"OK",&pos);
+}
 static rt_err_t AT_NSOCR(int localport)//create socket
 {
 	rt_err_t ret;
@@ -155,21 +178,103 @@ static rt_err_t AT_NSOCR(int localport)//create socket
 	scoketId = resp[0]-'0';
 	return RT_EOK;
 }
-static rt_err_t AT_NSOST(void)//send msg
+static rt_err_t AT_NSOST(const char* ip,const int remoteport,char* msg,int len)//send msg
 {
-
+	rt_err_t ret;
+	char* cmd0 = "AT+NSOST";
+	char cmd1[1024];
+	char resp[256],rlen=256,pos;
+	rt_sprintf(cmd1,"%s=%d,%s,%d,%d,%s\r\n",cmd0,socketId,ip,remoteport,len,msg);
+	ret = SendData(cmd1,strlen(cmd1));
+	if(ret)return ret;
+	ret = RecvData(resp,&rlen,2000);
+	if(ret)return ret;
+	return found_str(resp,"OK",&pos);
 }
-static rt_err_t AT_NSONMI(void)// recv msg interrupt
+static rt_err_t AT_NSONMI(char* msgflag, int* len)// recv msg interrupt
 {
-
+	rt_err_t ret;
+	char resp[1024],rlen=1024,pos,pos1;
+	ret = RecvData(resp,&rlen,10000);
+	if(ret)return ret;
+	ret= found_str(resp,"+NSONMI:",&pos);
+	if(ret != RT_EOK)return ret;
+	ret = found(resp+pos,"\r\n",&pos1);
+	if(ret != RT_EOK)
+	{
+		*len=0;
+		return ret;
+	}
+	rt_memcpy(msgflag,resp+pos,pos1-pos-2);
+	*len=pos1-pos-2;
+	return RT_EOK;
 }
-static rt_err_t AT_NSORF(void)//recv msg
+static rt_err_t AT_NSORF(char* msgflag,char* msg,int* len)//recv msg
 {
+	rt_err_t ret;
+	char* cmd0 = "AT+NSORF";
+	char cmd1[1024];
+	char resp[1024],rlen=1024,pos;
+	char* socket,ip,port,length,message;
+	char* substr;
+	int ilen;
+	rt_sprintf(cmd1,"%s=%s\r\n",cmd0,msgflag);
+	ret = SendData(cmd1,strlen(cmd1));
+	if(ret)return ret;
+	ret = RecvData(resp,&rlen,2000);
+	if(ret)return ret;
+	ret = found_str(resp,"OK",&pos);
+	if(ret)
+	{
+		*len =0;
+		return ret;
+	}
+	else
+	{
+		substr=resp;
 
+		socket=substr;
+		found_str(substr,',',&pos);
+		substr[pos-1] = '\0';
+		substr += pos;
+
+		ip=substr;
+		found_str(substr,',',&pos);
+		substr[pos-1] = '\0';
+		substr += pos;
+
+		port = substr+pos;
+		found_str(substr,',',&pos);
+		substr[pos-1] = '\0';
+		substr += pos;
+
+		length = substr+pos;
+		found_str(substr,',',&pos);
+		substr[pos-1] = '\0';
+		substr += pos;
+
+		message = substr+pos;
+		found_str(substr,',',&pos);
+		substr[pos-1] = '\0';
+		substr += pos;
+
+		ilen  =str2int(length);
+		rt_memcpy(msg,message,ilen);
+	}
+	return RT_EOK;
 }
 static rt_err_t AT_NSCL(void)
 {
-
+	rt_err_t ret;
+	char* cmd0 = "AT+NSCL";
+	char cmd1[1024];
+	char resp[256],rlen=256,pos;
+	rt_sprintf(cmd1,"%s=%d\r\n",cmd0,socketId);
+	ret = SendData(cmd1,strlen(cmd1));
+	if(ret)return ret;
+	ret = RecvData(resp,&rlen,2000);
+	if(ret)return ret;
+	return found_str(resp,"OK",&pos);
 }
 
 static rt_err_t rt_nb_iot_init(rt_device_t dev)
@@ -193,6 +298,9 @@ static rt_err_t rt_nb_iot_open(rt_device_t dev, rt_uint16_t oflag)
 		AT_NBAND(&band);
 		if(band != 5)return RT_EOK;
 	}
+	AT_NCONFIG();
+	AT_NSOCR(5683);
+
 	return RT_EOK;
 }
 static rt_err_t rt_nb_iot_close(rt_device_t dev)
@@ -202,34 +310,31 @@ static rt_err_t rt_nb_iot_close(rt_device_t dev)
 	return RT_EOK;
 }
 static rt_size_t rt_nb_iot_read(rt_device_t dev, rt_off_t pos, void *buffer, rt_size_t size)
-{
-	RT_ASSERT(dev != RT_NULL);
-	rt_uint32_t p_gpio_base = 0;
-	rt_uint32_t p_ctlbit_pos =0;
-	rt_uint32_t  *p_buf =(rt_uint32_t*) buffer ;
-	gpio_ctrl_num(dev, &p_gpio_base,&p_ctlbit_pos);
-	if (!(GPIO_EN(p_gpio_base)&(1<<p_ctlbit_pos)))
-		rt_kprintf("GPIO is not read mode \n");
-	*p_buf=(GPIO_IN(p_gpio_base)&(1<<p_ctlbit_pos))==0?0:1;
+{charchar
+	RT_ASSERT(dev != RT_NULL);char
+	rt_err_t ret;
+	char* msgflag[30];
+	int len;
+	AT_NSONMI();
+	ret=AT_NSOST("114.115.144.122",4567,buffer,size);
+	if(ret)
+	{
+		return  0;
+	}
+	return size;
 
 	return 1;
 }
 static rt_size_t rt_nb_iot_write(rt_device_t dev, rt_off_t pos, const void *buffer, rt_size_t size)
 {
 	RT_ASSERT(dev != RT_NULL);
-	rt_uint32_t p_gpio_base = 0;
-	rt_uint32_t p_ctlbit_pos =0;
-	rt_uint32_t  *p_buf =(rt_uint32_t*) buffer ;
-	gpio_ctrl_num(dev, &p_gpio_base,&p_ctlbit_pos);
-	if ((GPIO_EN(p_gpio_base)&(1<<p_ctlbit_pos)))
-		rt_kprintf("GPIO is not write mode \n");
-	if( *p_buf  == 1)
-		GPIO_OUT(p_gpio_base) |= (1<<p_ctlbit_pos);
-	else if(*p_buf  == 0)
-		GPIO_OUT(p_gpio_base) &= ~(1<<p_ctlbit_pos);
-	else
-		rt_kprintf("GPIO output value(%d) is wrong \n",*p_buf);
-	return 1;
+	rt_err_t ret;
+	ret=AT_NSOST("114.115.144.122",4567,buffer,size);
+	if(ret)
+	{
+		return  0;
+	}
+	return size;
 }
 
 
@@ -237,26 +342,6 @@ static rt_size_t rt_nb_iot_write(rt_device_t dev, rt_off_t pos, const void *buff
 rt_err_t  rt_nb_iot_control(rt_device_t dev, rt_uint8_t cmd, void *args)
 {
 	RT_ASSERT(dev != RT_NULL);
-	rt_uint32_t p_gpio_base = 0;
-	rt_uint32_t p_ctlbit_pos =0;
-	gpio_int_config_t *config;
-	if(gpio_ctrl_num(dev, &p_gpio_base,&p_ctlbit_pos) == RT_EIO)
-		return RT_EIO;
-
-	switch(cmd)
-	{
-		case GPIO_READ_MODE :
-			GPIO_EN(p_gpio_base) |= (1<<p_ctlbit_pos);//1= in
-			break;
-		case GPIO_WRITE_MODE :
-			GPIO_EN(p_gpio_base) &= ~(1<<p_ctlbit_pos);//0=out
-			break;
-		case GPIO_INT_MODE :
-			config =(gpio_int_config_t *) args;
-			gpio_int_set(dev,config->trigmethod,config->edge,config->level,config->isrName,config->handler);
-			break;
-		default: break;
-	}
 	return RT_EOK;
 }
 void rt_hw_nb_iot_init(void)
